cfa56e22efbd87870ea0b5c0ef49201c55cb0486
zoona <dev.hozoon@gmail.com>
2017-11-21 16:42:10 +0900

    url 변수처리, Bigdata Analysis 내용 변경

--- server/routes/api.js
+++ server/routes/api.js
@@ -15,15 +15,17 @@ let response = {
     message: null
 };
 
-router.get('/ds', (req, res) => {
+this.workingDir = '/Users/zoona/Work/Job/launchbag/LaunchBag';
 
+router.get('/ds', (req, res) => {
+    
     var socket = req.app.get('socketio');
     socket.emit('new-message', { message: "this is from api" });
     var spawn = require('child_process').spawn;
-
-
-    var child = spawn('ansible-playbook', ['-i', 'inventories/hosts', 'launchbag_datascience.yml'], {cwd: '/home/rts/apps/launchbag'});
-
+    
+    
+    var child = spawn('ansible-playbook', ['-i', 'inventories/hosts', 'launchbag_datascience.yml'], {cwd: this.workingDir});
+    
     var chunk = '';
     child.stdout.on("data", function(data) {
         chunk += data;
@@ -32,31 +34,53 @@ router.get('/ds', (req, res) => {
     child.stderr.on("data", function(data) {
         console.log("ps stderr: " + data);
     })
-
+    
     response.data = {"users": "many"};
     res.json(response);
 });
 
 router.get('/elk', (req, res) => {
     
-        var socket = req.app.get('socketio');
-        socket.emit('new-message', { message: "this is from api" });
-        var spawn = require('child_process').spawn;
+    var socket = req.app.get('socketio');
+    socket.emit('new-message', { message: "this is from api" });
+    var spawn = require('child_process').spawn;
     
     
-        var child = spawn('ansible-playbook', ['-i', 'inventories/hosts', 'launchbag_elk.yml'], {cwd: '/home/rts/apps/launchbag'});
+    var child = spawn('ansible-playbook', ['-i', 'inventories/hosts', 'launchbag_elk.yml'], {cwd: this.workingDir});
     
-        var chunk = '';
-        child.stdout.on("data", function(data) {
-            chunk += data;
-            socket.emit('elk', chunk);
-        });
-        child.stderr.on("data", function(data) {
-            console.log("ps stderr: " + data);
-        })
-    
-        response.data = {"users": "many"};
-        res.json(response);
+    var chunk = '';
+    child.stdout.on("data", function(data) {
+        chunk += data;
+        socket.emit('elk', chunk);
     });
+    child.stderr.on("data", function(data) {
+        console.log("ps stderr: " + data);
+    })
+    
+    response.data = {"users": "many"};
+    res.json(response);
+});
+
+router.get('/bda', (req, res) => {
+    
+    var socket = req.app.get('socketio');
+    socket.emit('new-message', { message: "this is from api" });
+    var spawn = require('child_process').spawn;
+    
+    
+    var child = spawn('ansible-playbook', ['-i', 'inventories/hosts', 'launchbag_zeppelin.yml', '-e', '"container_zepl_name=ZeppelinTest zepl_port=8089"'], {cwd: this.workingDir});
+    
+    var chunk = '';
+    child.stdout.on("data", function(data) {
+        chunk += data;
+        socket.emit('bda', chunk);
+    });
+    child.stderr.on("data", function(data) {
+        console.log("ps stderr: " + data);
+    })
+    
+    response.data = {"users": "many"};
+    res.json(response);
+});
 
 module.exports = router;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,61 +8,91 @@ import * as io from "socket.io-client";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, AfterViewChecked {
-
+  
   @ViewChild('scrollMe1') private myScrollContainer1: ElementRef;
   @ViewChild('scrollMe2') private myScrollContainer2: ElementRef;
-
+  @ViewChild('scrollMe3') private myScrollContainer3: ElementRef;
+  
   title = 'launchbag';
   message1: any;
   message2: any;
-  socket = io('http://169.56.88.43:3000');
+  message3: any;
+  socket = io('http://localhost:3000');
   finished1: boolean;
   finished2: boolean;
+  finished3: boolean;
   constructor(private http: Http){}
-
+  apiUrl = "http://localhost:3000/api/";
+  rancherUrl = 'http://tcl1:8080';
+  jupyterUrl = 'http://tcl2:8888';
+  kibanaUrl = "http://tcl2:5601/app/kibana#/dashboard/AV-R1w4qx1YW3m0zZ5qW?_g=(refreshInterval:('$$hashKey':'object:3758',display:'5%20seconds',pause:!f,section:1,value:5000),time:(from:now-15m,mode:quick,to:now))&_a=(description:'',filters:!(),options:(darkTheme:!f),panels:!((col:1,id:AV-R1FODx1YW3m0zZ5mT,panelIndex:1,row:1,size_x:6,size_y:3,type:visualization),(col:7,id:AV-R01pKx1YW3m0zZ5lE,panelIndex:2,row:1,size_x:6,size_y:3,type:visualization),(col:1,columns:!(customer_id,track_id,ismobile,listening_zip_code),id:AV-R1o4Nx1YW3m0zZ5pc,panelIndex:3,row:4,size_x:12,size_y:3,sort:!('@timestamp',desc),type:search)),query:(match_all:()),timeRestore:!f,title:dash-all,uiState:(),viewMode:view)";
+  zeppelinUrl = "http://tcl2:8089/#/notebook/2CZ99W966";
+  
   ngOnInit() {
     this.scrollToBottom1();
     this.scrollToBottom2();
+    this.scrollToBottom3();
     this.finished1 = false;
     this.finished2 = false;
+    this.finished3 = false;
+    
     this.socket.on('ds', function (data) {
       this.message1 = data;
       if(data.indexOf("RECAP") >= 0)
-        this.finished1 = true;
+      this.finished1 = true;
     }.bind(this));
+
     this.socket.on('elk', function (data) {
       this.message2 = data;
       if(data.indexOf("RECAP") >= 0)
-        this.finished2 = true;
+      this.finished2 = true;
+    }.bind(this));
+
+    this.socket.on('bda', function (data) {
+      this.message3 = data;
+      if(data.indexOf("RECAP") >= 0)
+      this.finished3 = true;
     }.bind(this));
   }
-
+  
   ngAfterViewChecked() {        
-    this.scrollToBottom1();       
-    this.scrollToBottom2();    
+    this.scrollToBottom1();
+    this.scrollToBottom2();
+    this.scrollToBottom3();
   } 
-
+  
   scrollToBottom1(): void {
-      try {
-          this.myScrollContainer1.nativeElement.scrollTop = this.myScrollContainer1.nativeElement.scrollHeight;
-      } catch(err) { }                 
+    try {
+      this.myScrollContainer1.nativeElement.scrollTop = this.myScrollContainer1.nativeElement.scrollHeight;
+    } catch(err) { }                 
   }
-
+  
   scrollToBottom2(): void {
     try {
-        this.myScrollContainer2.nativeElement.scrollTop = this.myScrollContainer2.nativeElement.scrollHeight;
+      this.myScrollContainer2.nativeElement.scrollTop = this.myScrollContainer2.nativeElement.scrollHeight;
     } catch(err) { }                 
-}
-
-  runDS() {
-    this.http.get("http://169.56.88.43:3000/api/ds")
-      .map(res => res.json())
-      .subscribe(d => console.log(d));
   }
-
+  scrollToBottom3(): void {
+    try {
+      this.myScrollContainer3.nativeElement.scrollTop = this.myScrollContainer3.nativeElement.scrollHeight;
+    } catch(err) { }                 
+  }
+  
+  runDS() {
+    this.http.get(this.apiUrl + "ds")
+    .map(res => res.json())
+    .subscribe(d => console.log(d));
+  }
+  
   runELK() {
-    this.http.get("http://169.56.88.43:3000/api/elk")
-      .map(res => res.json())
-      .subscribe(d => console.log(d));
+    this.http.get(this.apiUrl + "elk")
+    .map(res => res.json())
+    .subscribe(d => console.log(d));
+  }
+  
+  runBDA() {
+    this.http.get(this.apiUrl + "bda")
+    .map(res => res.json())
+    .subscribe(d => console.log(d));
   }
 }
